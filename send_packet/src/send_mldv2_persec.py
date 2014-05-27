615d7eaaad1939b5099bf18f086db70c2caedf22
t-koshiba <koshiba.tomohiro@gmail.com>
2014-05-27 15:21:54 +0900

    pep8対応

--- send_packet/src/send_mldv2_persec.py
+++ send_packet/src/send_mldv2_persec.py
@@ -1,11 +1,12 @@
 from ryu.app import simple_switch_13
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER
-from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import DEAD_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3, ofproto_v1_3_parser, ether, inet
 from ryu.lib.packet import packet, ethernet, ipv6, icmpv6, vlan
 from ryu.lib import hub
 import threading
+
 
 class SimpleMonitor(simple_switch_13.SimpleSwitch13):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -15,15 +16,14 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
 
     # message when connected switch
     msg = None
-    
+
     # send interval(sec)
-    WAIT_TIME = 5
-    
+    WAIT_TIME = 10
+
     def __init__(self, *args, **kwargs):
         super(SimpleMonitor, self).__init__(*args, **kwargs)
         self.datapaths = {}
         self.logger.debug('__init__ : %s', self.PROTPCOL)
-
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -48,14 +48,22 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
 
     def add_flow(self, datapath, priority, match, actions):
         self.logger.debug('add_flow STR : %s', self.PROTPCOL)
-        
-        actions = [ofproto_v1_3_parser.OFPActionOutput(ofproto_v1_3.OFPP_NORMAL)]
-        instructions = [ofproto_v1_3_parser.OFPInstructionActions(ofproto_v1_3.OFPIT_APPLY_ACTIONS, actions)]
+
+        actions = [
+            ofproto_v1_3_parser.OFPActionOutput(
+                ofproto_v1_3.OFPP_NORMAL)]
+        instructions = [
+            ofproto_v1_3_parser.OFPInstructionActions(
+                ofproto_v1_3.OFPIT_APPLY_ACTIONS, actions)]
         # match
-        #    match = ofproto_v1_3_parser.OFPMatch(eth_type=ether.ETH_TYPE_IPV6, ip_proto=inet.IPPROTO_ICMP6)
+        #    match = ofproto_v1_3_parser.OFPMatch
+        #           (eth_type=ether.ETH_TYPE_IPV6,
+        #            ip_proto=inet.IPPROTO_ICMP6)
         # miss match
-        match = ofproto_v1_3_parser.OFPMatch(eth_type=ether.ETH_TYPE_IPV6, ip_proto=inet.IPPROTO_ICMP)
-        flow_mod_msg = ofproto_v1_3_parser.OFPFlowMod(datapath, match=match, instructions=instructions)
+        match = ofproto_v1_3_parser.OFPMatch(
+            eth_type=ether.ETH_TYPE_IPV6, ip_proto=inet.IPPROTO_ICMP)
+        flow_mod_msg = ofproto_v1_3_parser.OFPFlowMod(
+            datapath, match=match, instructions=instructions)
         datapath.send_msg(flow_mod_msg)
 
         self.logger.debug('add_flow END : %s', self.PROTPCOL)
@@ -87,16 +95,22 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
             # get_protocols(pkt_icmpv6)
             pkt_icmpv6 = pkt.get_protocols(icmpv6.icmpv6)
             if 0 < len(pkt_icmpv6):
-                self.logger.debug('icmpv6= %s icmpv6.ND_NEIGHBOR_SOLICIT = %s' , str(pkt_icmpv6), icmpv6.ND_NEIGHBOR_SOLICIT)
-                
-                if not pkt_icmpv6[0].type_ in [icmpv6.MLDV2_LISTENER_REPORT, icmpv6.ICMPV6_MEMBERSHIP_QUERY]:
+                self.logger.debug(
+                    'icmpv6= %s icmpv6.ND_NEIGHBOR_SOLICIT = %s',
+                    str(pkt_icmpv6), icmpv6.ND_NEIGHBOR_SOLICIT)
+
+                if pkt_icmpv6[0].type_ not in [
+                        icmpv6.MLDV2_LISTENER_REPORT,
+                        icmpv6.ICMPV6_MEMBERSHIP_QUERY]:
                     print "icmpv6.type is " + str(pkt_icmpv6[0].type_)
                     return
-            
+
         dpid = datapath
         self.mac_to_port.setdefault(dpid, {})
 
-        self.logger.debug('packet in %s %s %s %s %s', dpid, src, dst, in_port, str(self.packet_in_cnt))
+        self.logger.debug(
+            'packet in %s %s %s %s %s',
+            dpid, src, dst, in_port, str(self.packet_in_cnt))
 
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
@@ -109,57 +123,73 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
         actions = [parser.OFPActionOutput(out_port)]
 
         # install a flow to avoid packet_in next time
-        self.logger.debug('in_port = %s, out_port = %s, OFPP_FLOOD = %s', str(in_port), str(out_port), str(ofproto.OFPP_FLOOD))
-        
+        self.logger.debug(
+            'in_port = %s, out_port = %s, OFPP_FLOOD = %s',
+            str(in_port), str(out_port), str(ofproto.OFPP_FLOOD))
+
         if out_port != ofproto.OFPP_FLOOD:
-            
+
             if 'eth' in self.PROTPCOL:
                 # match
-                match = parser.OFPMatch(in_port=in_port, eth_dst=dst )
-                #miss match
-                match = parser.OFPMatch(in_port=in_port, eth_type=0, eth_dst=dst )
+                match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+                # miss match
+                match = parser.OFPMatch(
+                    in_port=in_port, eth_type=0, eth_dst=dst)
             elif 'ipv6' in self.PROTPCOL:
-                match = parser.OFPMatch(in_port=in_port, eth_type=ether.ETH_TYPE_IPV6, ip_proto=inet.IPPROTO_ICMPV6, ipv6_dst=dst)
+                match = parser.OFPMatch(
+                    in_port=in_port, eth_type=ether.ETH_TYPE_IPV6,
+                    ip_proto=inet.IPPROTO_ICMPV6, ipv6_dst=dst)
 
             self.add_flow(datapath, 1, match, actions)
 
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
-        sendpkt = self.createPacket(src, dst, pkt_ipv6[0].src, pkt_ipv6[0].dst)
-        self.sendPacketOut(parser, datapath, in_port, actions, sendpkt.data)
-       
+        sendpkt = self.createPacket(
+            src, dst, pkt_ipv6[0].src, pkt_ipv6[0].dst)
+        self.sendPacketOut(
+            parser, datapath, in_port, actions, sendpkt.data)
 
     def createPacket(self, src, dst, srcip, dstip):
-        # create send packet 
+        # create send packet
         #   ether - vlan - ipv6 - icmpv6 ( - mldv2 )
         sendpkt = packet.Packet()
-        sendpkt.add_protocol(ethernet.ethernet(ethertype=ether.ETH_TYPE_8021Q, dst=dst, src=src))
-        sendpkt.add_protocol(vlan.vlan(pcp=0, cfi=0, vid=100, ethertype=ether.ETH_TYPE_IPV6))
-        sendpkt.add_protocol(ipv6.ipv6(src=srcip, dst=dstip, nxt=inet.IPPROTO_ICMPV6))
-        sendpkt.add_protocol(icmpv6.icmpv6(type_=icmpv6.ICMPV6_MEMBERSHIP_QUERY, 
-                                data=icmpv6.mldv2_query(address='::')))
+        sendpkt.add_protocol(ethernet.ethernet(
+            ethertype=ether.ETH_TYPE_8021Q, dst=dst, src=src))
+        sendpkt.add_protocol(vlan.vlan(
+            pcp=0, cfi=0, vid=100, ethertype=ether.ETH_TYPE_IPV6))
+        sendpkt.add_protocol(ipv6.ipv6(
+            src=srcip, dst=dstip, nxt=inet.IPPROTO_ICMPV6))
+        sendpkt.add_protocol(icmpv6.icmpv6(
+            type_=icmpv6.ICMPV6_MEMBERSHIP_QUERY,
+            data=icmpv6.mldv2_query(address='::')))
         sendpkt.serialize()
         return sendpkt
 
-
     def sendPacketOut(self, parser, datapath, in_port, actions, data):
-        #out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=data)
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=0xffffffff, in_port=in_port, actions=actions, data=data)
+        # out = parser.OFPPacketOut(
+        #    datapath=datapath, buffer_id=msg.buffer_id,
+        #    in_port=in_port, actions=actions, data=data)
+        out = parser.OFPPacketOut(
+            datapath=datapath, buffer_id=0xffffffff,
+            in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
 
-    @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    @set_ev_cls(
+        ofp_event.EventOFPStateChange,
+        [MAIN_DISPATCHER, DEAD_DISPATCHER])
     def _state_change_handler(self, ev):
         datapath = ev.datapath
         if ev.state == MAIN_DISPATCHER:
-            if not datapath.id in self.datapaths:
-                self.logger.debug('register datapath: %016x', datapath.id)
+            if datapath.id not in self.datapaths:
+                self.logger.debug(
+                    'register datapath: %016x', datapath.id)
                 self.datapaths[datapath.id] = datapath
         elif ev.state == DEAD_DISPATCHER:
             if datapath.id in self.datapaths:
-                self.logger.debug('unregister datapath: %016x', datapath.id)
+                self.logger.debug(
+                    'unregister datapath: %016x', datapath.id)
                 del self.datapaths[datapath.id]
-
 
     def _send_regularly(self):
         datapath = self.msg.datapath
@@ -169,20 +199,24 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
         src = "11:22:33:44:55:66"
         dst = "66:55:44:33:22:11"
         srcip = "11::"
-        dstip= "::11"
+        dstip = "::11"
         in_port = 1
-        
+
         sendpkt = self.createPacket(src, dst, srcip, dstip)
         while True:
             # wait for regist datapath before send
-            if not datapath.id in self.datapaths: hub.sleep(1)
-            else: break
-        
+            if datapath.id not in self.datapaths:
+                hub.sleep(1)
+            else:
+                break
+
         while True:
             # stop send when delete datapath
-            if not datapath.id in self.datapaths: break
-            
-            self.sendPacketOut(parser, datapath, in_port, actions, sendpkt.data)
-            self.logger.debug("******** send packet :\n %s\n" % (sendpkt,))
-            hub.sleep(self.WAIT_TIME)
+            if datapath.id not in self.datapaths:
+                break
 
+            self.sendPacketOut(
+                parser, datapath, in_port, actions, sendpkt.data)
+            self.logger.debug(
+                "******** send packet :\n %s\n" % (sendpkt,))
+            hub.sleep(self.WAIT_TIME)
